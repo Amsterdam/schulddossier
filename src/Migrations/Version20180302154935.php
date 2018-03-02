695a8f67c642eb7df36b3ce59971b652b5a25187
Maarten de Keizer <github@maartendekeizer.nl>
2018-03-02 17:01:19 +0100

    Sessie via PostgreSQL database omdat Datapunt geen shared file system, Redis of memcached ondersteund voor sessiedata

---
+++ src/Migrations/Version20180302154935.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180302154935 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql('CREATE TABLE sessions (
+            sess_id VARCHAR(128) NOT NULL PRIMARY KEY,
+            sess_data BYTEA NOT NULL,
+            sess_time INTEGER NOT NULL,
+            sess_lifetime INTEGER NOT NULL
+        )');
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql('DROP TABLE sessions');
+    }
+}
