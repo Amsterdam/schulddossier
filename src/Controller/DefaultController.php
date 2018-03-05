4972cd1ade8b98a0953e774ec167155ba567fcb3
Maarten de Keizer <github@maartendekeizer.nl>
2018-03-05 21:38:44 +0100

    Speciale test voor PPK

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace GemeenteAmsterdam\FixxxSchuldhulp\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class DefaultController
 {
@@ -13,5 +15,30 @@ class DefaultController
     public function indexAction(Request $request)
     {
         return new Response($request->query->has('msg') === false ? 'Hoi' : $request->query->get('msg'));
+    }
+
+    /**
+     * @Route("/special/for/ppk")
+     */
+    public function uploadTestAction(Request $request)
+    {
+        if ($request->getMethod() === 'POST') {
+            $data = [];
+            foreach ($request->files->all() as $form) {
+                foreach ($form as $file) {
+                    /** @var $file UploadedFile */
+                    $data[] = [
+                        'errorInt' => $file->getError(),
+                        'errorMsg' => $file->getErrorMessage(),
+                        'name' => $file->getClientOriginalName(),
+                        'ext' => $file->getClientOriginalExtension(),
+                        'mime' => $file->getClientMimeType(),
+                        'size' => $file->getClientSize()
+                    ];
+                }
+            }
+            return new JsonResponse($data);
+        }
+        return new Response('<html><body><form method="post" enctype="multipart/form-data"><input type="file" name="specialFormForPpk[eenBestand]"><button type="submit">Stuur op</button></form></body></html>');
     }
 }
