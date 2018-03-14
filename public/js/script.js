158892d09748d29660a74077172e2ba56e8df447
Maarten de Keizer <github@maartendekeizer.nl>
2018-03-14 11:34:13 +0100

    Toon flash messages

--- public/js/script.js
+++ public/js/script.js
@@ -306,3 +306,19 @@ function findPos(obj) {
 	}
 	return [curleft,curtop];
 }
+
+window.schuldhulp = window.schuldhulp || {};
+
+window.schuldhulp.flashMessage = function (flashDom) {
+        flashDom.querySelector('a[href="#close-flash"]').addEventListener('click', (function (event) {
+            flashDom.remove();
+            event.preventDefault();
+        }));
+    };
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    var flashDoms = document.getElementsByClassName('flash-message');
+    for (var i = 0; i < flashDoms.length; i ++) {
+        new window.schuldhulp.flashMessage(flashDoms.item(i));
+    }
+});
