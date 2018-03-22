b756c70003fd3d8c1b99cbe0b58fca48144d6e9c
Maarten de Keizer <github@maartendekeizer.nl>
2018-03-22 14:04:12 +0100

    Nieuwe documenten bij update ondersteunen

--- src/Controller/AppDossierController.php
+++ src/Controller/AppDossierController.php
@@ -517,6 +517,24 @@ class AppDossierController extends Controller
         $schuldItem->setBewerkDatumTijd(new \DateTime());
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $files = $form->get('file')->getData();
+            foreach ($files as $file) {
+                /** @var $file File */
+                $document = new Document();
+                $document->setFile($file);
+                $document->setMd5Hash(md5($document->getFile()->getRealPath()));
+                $document->setMainTag('dossier-' . $dossier->getId());
+                $document->setNaam('naamloos');
+                $document->setGroep('dossier');
+                $document->setUploader($this->getUser());
+                $document->setUploadDatumTijd(new \DateTime());
+                $dossierDocument = new DossierDocument();
+                $dossierDocument->setDocument($document);
+                $dossierDocument->setDossier($dossier);
+                $dossierDocument->setOnderwerp('schuldenoverzicht');
+                $dossierDocument->setSchuldItem($schuldItem);
+            }
+
             $em->flush();
 
             return new JsonResponse($this->get('json_serializer')->normalize($schuldItem));
