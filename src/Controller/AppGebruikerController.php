2aab55565c22c6ed451cd1e02b41538f2a33423f
Maarten de Keizer <github@maartendekeizer.nl>
2018-03-26 17:47:40 +0200

    Terugzetten max items in gebruikerslijst

--- src/Controller/AppGebruikerController.php
+++ src/Controller/AppGebruikerController.php
@@ -26,7 +26,7 @@ class AppGebruikerController extends Controller
         /** @var $repository GebruikerRepository */
         $repository = $em->getRepository(Gebruiker::class);
 
-        $maxPageSize = 1;
+        $maxPageSize = 10;
 
         $gebruikers = $repository->findAll($request->query->getInt('page', 0), $request->query->getInt('pageSize', $maxPageSize));
 
