5fb535685e453f2b6e03f1fab10dd9594bf74f99
Maarten de Keizer <github@maartendekeizer.nl>
2018-03-28 08:09:12 +0200

    Verwijderen oude code PPK

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -17,32 +17,4 @@ class DefaultController extends Controller
     {
         return new Response($request->query->has('msg') === false ? 'Hoi' : $request->query->get('msg'));
     }
-
-    /**
-     * @Route("/app/special/for/ppk")
-     */
-    public function uploadTestAction(Request $request)
-    {
-        return $this->render('Dossier/schuldenlijst/index.html.twig', [
-
-		]);
-        if ($request->getMethod() === 'POST') {
-            $data = [];
-            foreach ($request->files->all() as $form) {
-                foreach ($form as $file) {
-                    /** @var $file UploadedFile */
-                    $data[] = [
-                        'errorInt' => $file->getError(),
-                        'errorMsg' => $file->getErrorMessage(),
-                        'name' => $file->getClientOriginalName(),
-                        'ext' => $file->getClientOriginalExtension(),
-                        'mime' => $file->getClientMimeType(),
-                        'size' => $file->getClientSize()
-                    ];
-                }
-            }
-            return new JsonResponse($data);
-        }
-        return new Response('<html><body><form method="post" enctype="multipart/form-data"><input type="file" name="specialFormForPpk[eenBestand]"><button type="submit">Stuur op</button></form></body></html>');
-    }
 }
