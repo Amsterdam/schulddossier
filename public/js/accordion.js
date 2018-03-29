203bed6cca8e12e90f674d50e5b9b4ff5e88a144
Maarten de Keizer <github@maartendekeizer.nl>
2018-03-29 11:02:05 +0200

    Fix voor accordion die niet openklapt bij schuldenlijst

--- public/js/accordion.js
+++ public/js/accordion.js
@@ -1,8 +1,7 @@
 window.schuldhulp = window.schuldhulp || {};
 
-
 document.addEventListener('click', function (event) {
-    if (window.schuldhulp._.matches(event.target, '.accordion .accordion-header a')) {
+    if (window.schuldhulp._.matches(event.target, '.accordion .accordion-header a, .accordion .accordion-header a *')) {
         event.preventDefault();
         var accordionDom = window.schuldhulp._.findParent(event.target, '.accordion');
         var accordionContainer = window.schuldhulp._.findParent(event.target, '.accordion-container');
