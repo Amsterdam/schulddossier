cf02fe5aa3cbb75e10a8748123cb1a70b920e1cf
Maarten de Keizer <github@maartendekeizer.nl>
2018-04-11 11:55:30 +0200

    Schuldeiser beheer interface

---
+++ src/Migrations/Version20180411093516.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180411093516 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE schuldeiser ADD allegro_code VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE schuldeiser ADD straat VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE schuldeiser ADD huisnummer VARCHAR(25) DEFAULT NULL');
+        $this->addSql('ALTER TABLE schuldeiser ADD huisnummer_toevoeging VARCHAR(25) DEFAULT NULL');
+        $this->addSql('ALTER TABLE schuldeiser ADD postcode VARCHAR(6) DEFAULT NULL');
+        $this->addSql('ALTER TABLE schuldeiser ADD plaats VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE schuldeiser ADD opmerkingen TEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE schuldeiser DROP allegro_code');
+        $this->addSql('ALTER TABLE schuldeiser DROP straat');
+        $this->addSql('ALTER TABLE schuldeiser DROP huisnummer');
+        $this->addSql('ALTER TABLE schuldeiser DROP huisnummer_toevoeging');
+        $this->addSql('ALTER TABLE schuldeiser DROP postcode');
+        $this->addSql('ALTER TABLE schuldeiser DROP plaats');
+        $this->addSql('ALTER TABLE schuldeiser DROP opmerkingen');
+    }
+}
