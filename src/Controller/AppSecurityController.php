c30d668b2f0d7e4b88af46f818f800a1d60cd55a
Maarten de Keizer <github@maartendekeizer.nl>
2018-11-19 15:46:54 +0100

    Wachtwoord aanpassen formulier

--- src/Controller/AppSecurityController.php
+++ src/Controller/AppSecurityController.php
@@ -6,6 +6,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Doctrine\ORM\EntityManagerInterface;
+use GemeenteAmsterdam\FixxxSchuldhulp\Form\Type\GebruikerChangePasswordFormType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class AppSecurityController extends Controller
 {
@@ -29,5 +32,29 @@ class AppSecurityController extends Controller
     public function logoutAction()
     {
         //
+    }
+
+    /**
+     * @Route("/app/wachtwoord-veranderen")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function changePasswordAction(Request $request, EntityManagerInterface $em)
+    {
+        $gebruiker = $this->getUser();
+
+        $form = $this->createForm(GebruikerChangePasswordFormType::class, $gebruiker);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Wachtwoord aangepast!');
+            return $this->redirectToRoute('gemeenteamsterdam_fixxxschuldhulp_default_appredirect', []);
+        }
+
+        return $this->render('Security/changePassword.html.twig', [
+            'gebruiker' => $gebruiker,
+            'form' => $form->createView(),
+        ]);
     }
 }
---
+++ src/Form/Type/GebruikerChangePasswordFormType.php
@@ -0,0 +1,58 @@
+<?php
+namespace GemeenteAmsterdam\FixxxSchuldhulp\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Gebruiker;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\Security\Core\Security;
+
+class GebruikerChangePasswordFormType extends AbstractType
+{
+    /**
+     * @var Security
+     */
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('currentPassword', PasswordType::class, [
+            'required' => true,
+            'mapped' => false,
+            'label' => 'Huidig wachtwoord',
+            'constraints' => [
+                new UserPassword()
+            ]
+        ]);
+        $builder->add('clearPassword', RepeatedType::class, [
+            'required' => false,
+            'type' => PasswordType::class,
+            'first_options' => ['label' => 'Nieuw wachtwoord'],
+            'second_options' => ['label' => 'Nieuw wachtwoord (herhaal)'],
+        ]);
+
+        $user = $this->security->getUser();
+
+        $builder->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) {
+            if ($event->getForm()->get('clearPassword')->getData() !== null && $event->getForm()->get('clearPassword')->getData() !== '') {
+                $event->getData()->setPasswordChangedDateTime(new \DateTime());
+            }
+        });
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefault('data_class', Gebruiker::class);
+        $resolver->setDefault('choice_translation_domain', false);
+    }
+}
