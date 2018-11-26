9ed71b8a2496dfb731f53ec89988bedc494214cd
Caroga <roberto@caroga.net>
2018-11-26 11:34:16 +0100

    Fixed incorrect import statement

--- src/Migrations/Version20181119121437.php
+++ src/Migrations/Version20181119121437.php
@@ -3,7 +3,7 @@
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\Migrations\AbstractMigration;
+use Doctrine\DBAL\Migrations\AbstractMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
