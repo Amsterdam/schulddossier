372382a1142f58a916116d24b67d30857260db21
Maarten de Keizer <github@maartendekeizer.nl>
2018-12-03 17:00:48 +0100

    Sorteren teams

--- src/Entity/Team.php
+++ src/Entity/Team.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="GemeenteAmsterdam\FixxxSchuldhulp\Repository\TeamRepository")
  * @ORM\Table
  */
 class Team
