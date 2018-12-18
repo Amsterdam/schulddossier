1513fa8f9a7f436e78c0fc26670db2d1db1ae4c8
Caroga <roberto@caroga.net>
2018-12-18 15:14:34 +0100

    Added basic logviewer, only for ROLE_APPBEHEER and ROLE_ADMIN

---
+++ src/Controller/AppLogController.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GemeenteAmsterdam\FixxxSchuldhulp\Controller;
+
+use GemeenteAmsterdam\FixxxSchuldhulp\Entity\ActionEvent;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * @Route("/app/log")
+ * @Security("has_role('ROLE_APPBEHEER') || has_role('ROLE_ADMIN')")
+ */
+class AppLogController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Security("has_role('ROLE_APPBEHEER') || has_role('ROLE_ADMIN')")
+     */
+    public function indexAction()
+    {
+        $logs = $this->getDoctrine()
+            ->getRepository(ActionEvent::class)
+            ->findBy([], null, 30);
+
+        if (!$logs) {
+            throw $this->createNotFoundException(
+                'No logs found '
+            );
+        }
+
+        return $this->render('Log/index.html.twig', ['logs' => $logs]);
+    }
+}
