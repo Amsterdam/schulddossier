b398d10a81fc623216db03beb258d4a8099fcc0c
Caroga <roberto@caroga.net>
2018-12-19 15:24:35 +0100

    Make schuldhulpburaeu required when creating a new user with the role MADI

--- src/Form/Type/GebruikerFormType.php
+++ src/Form/Type/GebruikerFormType.php
@@ -1,24 +1,24 @@
 <?php
+
 namespace GemeenteAmsterdam\FixxxSchuldhulp\Form\Type;
 
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Gebruiker;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Schuldhulpbureau;
 use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Team;
-use Doctrine\ORM\EntityRepository;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\FormEvents;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use GemeenteAmsterdam\FixxxSchuldhulp\Validator\Constraints\RequiredFieldsWhenAddingNewMadiUser;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Security;
-use Psr\Log\LoggerInterface;
 
 class GebruikerFormType extends AbstractType
 {
@@ -90,6 +90,7 @@ class GebruikerFormType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver)
     {
+        $resolver->setDefault('constraints', [new RequiredFieldsWhenAddingNewMadiUser()]);
         $resolver->setDefault('data_class', Gebruiker::class);
         $resolver->setDefault('choice_translation_domain', false);
     }
---
+++ src/Validator/Constraints/RequiredFieldsWhenAddingNewMadiUserValidator.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GemeenteAmsterdam\FixxxSchuldhulp\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * Class RequiredFieldsWhenAddingNewMadiUserValidator
+ *
+ * @package GemeenteAmsterdam\FixxxSchuldhulp\Validator\Constraints
+ */
+class RequiredFieldsWhenAddingNewMadiUserValidator extends ConstraintValidator
+{
+
+    /**
+     * Checks if the passed value is valid.
+     *
+     * @param mixed      $protocol   The value that should be validated
+     * @param Constraint $constraint The constraint for the validation
+     */
+    public function validate($protocol, Constraint $constraint): void
+    {
+        if (strtolower($protocol->getType()) === 'madi') {
+            if (is_null($protocol->getSchuldhulpbureau())) {
+                $this->context->buildViolation($constraint->message)
+                    ->atPath('schuldhulpbureau')
+                    ->addViolation();
+            }
+        }
+    }
+}
