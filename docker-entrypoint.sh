f8aacbde82342a5b4041ce5838c4e938df99c09f
a.obdeijn <A.Obdeijn@spilgames.com>
2019-01-31 15:23:06 +0100

    Fix logging to Kibana, point to the right log locations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -34,8 +34,8 @@ cp docker/nginx/localhost.key /srv/localhost.key
 cp docker/php/extra-$APP_ENV.ini /usr/local/etc/php/conf.d/99-extra.ini
 
 tail -f var/log/dev.log &
-tail -f var/log/acceptance.log &
-tail -f var/log/production.log &
+tail -f /srv/app/var/log/acceptance.log &
+tail -f /srv/app/var/log/production.log &
 
 nginx
 php-fpm -F
