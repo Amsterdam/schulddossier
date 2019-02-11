522bec9fcffde4698d869c532143e5fbe63ec93a
Roberto Gardenier <roberto@caroga.net>
2019-02-11 17:03:51 +0100

    Added migration to add custom schuldeiser `schuldeiser onbekend (#37)

---
+++ src/Migrations/Version20190211151259.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+use Doctrine\ORM\EntityManagerInterface;
+use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Schuldeiser;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190211151259 extends AbstractMigration
+{
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('INSERT INTO schuldeiser (id, bedrijfsnaam, rekening) VALUES (nextval(\'schuldeiser_id_seq\'), \'`onbekende schuldeiser\', \'0\')');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DELETE FROM schuldeiser WHERE bedrijfsnaam = \'`onbekende schuldeiser\';');
+    }
+
+}
