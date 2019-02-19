5ec077bfcdd78ce024a8d7ee8ecfe91df4ca16ae
Caroga <roberto@caroga.net>
2019-02-19 15:14:27 +0100

    Refactored transformation of MedewerkerSchuldhulpbureau to events

---
+++ src/Form/Event/TransformMedewerkerSchuldhulpbureau.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GemeenteAmsterdam\FixxxSchuldhulp\Form\Event;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityNotFoundException;
+use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Gebruiker;
+use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Schuldhulpbureau;
+use GemeenteAmsterdam\FixxxSchuldhulp\Form\Type\SchuldeiserType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+class TransformMedewerkerSchuldhulpbureau implements EventSubscriberInterface
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * TransformMedewerkerSchuldhulpbureau constructor.
+     *
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @inheritdoc
+     * @return array
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            FormEvents::PRE_SET_DATA => 'onPreSetData',
+            FormEvents::PRE_SUBMIT => 'onPreSubmit',
+        ];
+    }
+
+    /**
+     * @param FormEvent $event
+     */
+    public function onPreSetData(FormEvent $event)
+    {
+        $form = $event->getForm();
+        $data = $event->getData();
+
+        $schuldhulpbureau = $data->getSchuldhulpbureau()->getId();
+        $medewerkerSchuldhulpbureau = $data->getMedewerkerSchuldhulpbureau()->getId();
+        $schuldHulpbureauGebruiker = $schuldhulpbureau . '_' . $medewerkerSchuldhulpbureau;
+
+        $form->add('schuldHulpbureauGebruiker', SchuldeiserType::class, [
+            'required' => true,
+            'multiple' => false,
+            'expanded' => false,
+            'mapped' => false,
+            'data' => $schuldHulpbureauGebruiker
+        ])->setData($schuldHulpbureauGebruiker);
+    }
+
+    /**
+     * @param FormEvent $event
+     *
+     * @throws EntityNotFoundException
+     */
+    public function onPreSubmit(FormEvent $event)
+    {
+        $form = $event->getForm();
+        $data = $event->getData();
+
+        $ids = explode('_', $data['schuldHulpbureauGebruiker']);
+
+        $schuldhulpbureau = $this->entityManager->getRepository(Schuldhulpbureau::class)->find($ids[0]);
+        if (empty($schuldhulpbureau)) {
+            throw new EntityNotFoundException('Schuldhulpbureau with the following id does not exist: ' . $ids[0]);
+        }
+
+        $medewerkerSchuldhulpbureau = $this->entityManager->getRepository(Gebruiker::class)->find($ids[1]);
+        if (empty($medewerkerSchuldhulpbureau)) {
+            throw new EntityNotFoundException('Gebruiker with the following id does not exist: ' . $ids[1]);
+        }
+
+        $data['schuldhulpbureau'] = $schuldhulpbureau->getId();
+        $data['medewerkerSchuldhulpbureau'] = $medewerkerSchuldhulpbureau->getId();
+
+        $form->add('schuldhulpbureau', EntityType::class, [
+            'required' => true,
+            'class' => Schuldhulpbureau::class,
+            'multiple' => false,
+            'expanded' => false,
+        ])->setData($schuldhulpbureau->getId());
+
+        $form->add('medewerkerSchuldhulpbureau', EntityType::class, [
+            'required' => true,
+            'class' => Gebruiker::class,
+            'multiple' => false,
+        ])->setData($medewerkerSchuldhulpbureau->getId());
+
+        $event->setData($data);
+    }
+}
--- src/Form/Type/DetailDossierFormType.php
+++ src/Form/Type/DetailDossierFormType.php
@@ -2,32 +2,25 @@
 
 namespace GemeenteAmsterdam\FixxxSchuldhulp\Form\Type;
 
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\EntityNotFoundException;
 use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Dossier;
-use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Gebruiker;
-use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Schuldhulpbureau;
 use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Team;
+use GemeenteAmsterdam\FixxxSchuldhulp\Form\Event\TransformMedewerkerSchuldhulpbureau;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class DetailDossierFormType extends AbstractType
 {
-
     /**
-     * @var EntityManagerInterface
+     * @var TransformMedewerkerSchuldhulpbureau
      */
-    private $entityManager;
+    private $medewerkerSchuldhulpbureau;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(TransformMedewerkerSchuldhulpbureau $medewerkerSchuldhulpbureau)
     {
-
-        $this->entityManager = $entityManager;
+        $this->medewerkerSchuldhulpbureau = $medewerkerSchuldhulpbureau;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -41,74 +34,24 @@ class DetailDossierFormType extends AbstractType
             'required' => false
         ]);
 
-        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
-            $form = $event->getForm();
-            /** @var Dossier $data */
-            $data = $event->getData();
-
-            $schuldhulpbureau = $data->getSchuldhulpbureau()->getId();
-            $medewerkerSchuldhulpbureau = $data->getMedewerkerSchuldhulpbureau()->getId();
-            $schuldHulpbureauGebruiker = $schuldhulpbureau . '_' . $medewerkerSchuldhulpbureau;
-
-            $form->add('schuldHulpbureauGebruiker', SchuldeiserType::class, [
-                'required' => true,
-                'multiple' => false,
-                'expanded' => false,
-                'mapped' => false,
-                'data' => $schuldHulpbureauGebruiker
-            ])->setData($schuldHulpbureauGebruiker);
-        });
-
-
-        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
-            $form = $event->getForm();
-            $data = $event->getData();
-
-            $ids = explode('_', $data['schuldHulpbureauGebruiker']);
-
-            $schuldhulpbureau = $this->entityManager->getRepository(Schuldhulpbureau::class)->find($ids[0]);
-            if (empty($schuldhulpbureau)) {
-                throw new EntityNotFoundException('Schuldhulpbureau with the following id does not exist: ' . $ids[0]);
-            }
-
-            $medewerkerSchuldhulpbureau = $this->entityManager->getRepository(Gebruiker::class)->find($ids[1]);
-            if (empty($medewerkerSchuldhulpbureau)) {
-                throw new EntityNotFoundException('Gebruiker with the following id does not exist: ' . $ids[1]);
-            }
-
-            $data['schuldhulpbureau'] = $schuldhulpbureau->getId();
-            $data['medewerkerSchuldhulpbureau'] = $medewerkerSchuldhulpbureau->getId();
-
-            $form->add('schuldhulpbureau', EntityType::class, [
-                'required' => true,
-                'class' => Schuldhulpbureau::class,
-                'multiple' => false,
-                'expanded' => false,
-            ])->setData($schuldhulpbureau->getId());
-
-            $form->add('medewerkerSchuldhulpbureau', EntityType::class, [
-                'required' => true,
-                'class' => Gebruiker::class,
-                'multiple' => false,
-            ])->setData($medewerkerSchuldhulpbureau->getId());
-
-            $event->setData($data);
-        });
-
         $builder->add('teamGka', EntityType::class, [
             'required' => false,
             'class' => Team::class,
             'multiple' => false,
             'expanded' => false
         ]);
+
         $builder->add('regasNummer', TextType::class, [
             'required' => false,
             'label' => 'MaDi dossiernr.'
         ]);
+
         $builder->add('allegroNummer', TextType::class, [
             'required' => false,
             'label' => 'GKA dossiernr.'
         ]);
+
+        $builder->addEventSubscriber($this->medewerkerSchuldhulpbureau);
     }
 
     public function configureOptions(OptionsResolver $resolver)
