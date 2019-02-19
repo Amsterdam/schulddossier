0213f8d49516786771605c4f1a2809e3de5b5305
Caroga <roberto@caroga.net>
2019-02-19 14:55:15 +0100

    New selectbox for now supports saving schuldhulpbureau and medewerkerSchuldhulpbureau with 1 select option

--- src/Form/Type/DetailDossierFormType.php
+++ src/Form/Type/DetailDossierFormType.php
@@ -2,7 +2,8 @@
 
 namespace GemeenteAmsterdam\FixxxSchuldhulp\Form\Type;
 
-use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityNotFoundException;
 use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Dossier;
 use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Gebruiker;
 use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Schuldhulpbureau;
@@ -11,10 +12,24 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class DetailDossierFormType extends AbstractType
 {
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+
+        $this->entityManager = $entityManager;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('clientNaam', TextType::class, [
@@ -25,25 +40,61 @@ class DetailDossierFormType extends AbstractType
             'label' => 'Partnernaam',
             'required' => false
         ]);
-        $builder->add('schuldhulpbureau', EntityType::class, [
-            'required' => true,
-            'class' => Schuldhulpbureau::class,
-            'multiple' => false,
-            'expanded' => false
-        ]);
 
-        $builder->add('medewerkerSchuldhulpbureau', SchuldeiserType::class, [
-            'required' => true,
-            'multiple' => false,
-            'expanded' => false,
-            'query_builder' => function (EntityRepository $repository) {
-                $qb = $repository->createQueryBuilder('gebruiker');
-                $qb->andWhere('gebruiker.type = :type');
-                $qb->setParameter('type', Gebruiker::TYPE_MADI);
-                $qb->addOrderBy('gebruiker.username', 'ASC');
-                return $qb;
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $form = $event->getForm();
+            /** @var Dossier $data */
+            $data = $event->getData();
+
+            $schuldhulpbureau = $data->getSchuldhulpbureau()->getId();
+            $medewerkerSchuldhulpbureau = $data->getMedewerkerSchuldhulpbureau()->getId();
+            $schuldHulpbureauGebruiker = $schuldhulpbureau . '_' . $medewerkerSchuldhulpbureau;
+
+            $form->add('schuldHulpbureauGebruiker', SchuldeiserType::class, [
+                'required' => true,
+                'multiple' => false,
+                'expanded' => false,
+                'mapped' => false,
+                'data' => $schuldHulpbureauGebruiker
+            ])->setData($schuldHulpbureauGebruiker);
+        });
+
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+            $form = $event->getForm();
+            $data = $event->getData();
+
+            $ids = explode('_', $data['schuldHulpbureauGebruiker']);
+
+            $schuldhulpbureau = $this->entityManager->getRepository(Schuldhulpbureau::class)->find($ids[0]);
+            if (empty($schuldhulpbureau)) {
+                throw new EntityNotFoundException('Schuldhulpbureau with the following id does not exist: ' . $ids[0]);
             }
-        ]);
+
+            $medewerkerSchuldhulpbureau = $this->entityManager->getRepository(Gebruiker::class)->find($ids[1]);
+            if (empty($medewerkerSchuldhulpbureau)) {
+                throw new EntityNotFoundException('Gebruiker with the following id does not exist: ' . $ids[1]);
+            }
+
+            $data['schuldhulpbureau'] = $schuldhulpbureau->getId();
+            $data['medewerkerSchuldhulpbureau'] = $medewerkerSchuldhulpbureau->getId();
+
+            $form->add('schuldhulpbureau', EntityType::class, [
+                'required' => true,
+                'class' => Schuldhulpbureau::class,
+                'multiple' => false,
+                'expanded' => false,
+            ])->setData($schuldhulpbureau->getId());
+
+            $form->add('medewerkerSchuldhulpbureau', EntityType::class, [
+                'required' => true,
+                'class' => Gebruiker::class,
+                'multiple' => false,
+            ])->setData($medewerkerSchuldhulpbureau->getId());
+
+            $event->setData($data);
+        });
+
         $builder->add('teamGka', EntityType::class, [
             'required' => false,
             'class' => Team::class,
