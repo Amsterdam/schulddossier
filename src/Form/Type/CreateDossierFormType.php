ce3a9e630767c85a777b9b0e9344380fd820ec44
Roberto Gardenier <roberto@caroga.net>
2019-02-20 10:43:15 +0100

    WIP: Feature/madi filters (#39)
    * WIP: first version showing dropdown with bureaus and gebruikers.
    
    * New selectbox for now supports saving schuldhulpbureau and medewerkerSchuldhulpbureau with 1 select option
    
    * Refactored transformation of MedewerkerSchuldhulpbureau to events
    
    * Added medewerkerSchuldhulpbureau selectbox to CreateDossierFormtype
    
    * Admin/GKA will now see all gebruikers from all schuldhulpbureaus

--- src/Form/Type/CreateDossierFormType.php
+++ src/Form/Type/CreateDossierFormType.php
@@ -1,61 +1,49 @@
 <?php
+
 namespace GemeenteAmsterdam\FixxxSchuldhulp\Form\Type;
 
+use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Dossier;
+use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Team;
+use GemeenteAmsterdam\FixxxSchuldhulp\Form\Event\TransformMedewerkerSchuldhulpbureau;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Dossier;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Schuldhulpbureau;
-use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Team;
-use Doctrine\ORM\EntityRepository;
-use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Gebruiker;
 
+/**
+ * Class CreateDossierFormType
+ *
+ * @package GemeenteAmsterdam\FixxxSchuldhulp\Form\Type
+ */
 class CreateDossierFormType extends AbstractType
 {
+    /**
+     * @var TransformMedewerkerSchuldhulpbureau
+     */
+    private $medewerkerSchuldhulpbureau;
+
+    public function __construct(TransformMedewerkerSchuldhulpbureau $medewerkerSchuldhulpbureau)
+    {
+        $this->medewerkerSchuldhulpbureau = $medewerkerSchuldhulpbureau;
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('clientNaam', TextType::class, [
             'label' => 'Cliëntnaam',
             'required' => true
         ]);
+
         $builder->add('partnerNaam', TextType::class, [
             'label' => 'Partnernaam',
             'required' => false
         ]);
-        $builder->add('schuldhulpbureau', EntityType::class, [
-            'placeholder' => '- Kies een schuldhulpbureau -',
-            'required' => true,
-            'class' => Schuldhulpbureau::class,
-            'multiple' => false,
-            'expanded' => false,
-            'choice_attr' => function (Schuldhulpbureau $schuldhulpbureau) {
-                if ($schuldhulpbureau->getStandaardGkaTeam() !== null) {
-                    return ['data-gka-team' => $schuldhulpbureau->getId()];
-                }
-                return [];
-            }
-        ]);
-        $builder->add('medewerkerSchuldhulpbureau', EntityType::class, [
-            'required' => true,
-            'class' => Gebruiker::class,
-            'multiple' => false,
-            'expanded' => false,
-            'query_builder' => function (EntityRepository $repository) {
-                $qb = $repository->createQueryBuilder('gebruiker');
-                $qb->andWhere('gebruiker.type = :type');
-                $qb->setParameter('type', Gebruiker::TYPE_MADI);
-                $qb->addOrderBy('gebruiker.username', 'ASC');
-                return $qb;
-            },
-            'group_by' => function (Gebruiker $gebruiker) {
-                if ($gebruiker->getSchuldhulpbureaus()->count() > 0) {
-                    return $gebruiker->getSchuldhulpbureaus()->first()->__toString();
-                }
-                return null;
-            },
-        ]);
+
         $builder->add('teamGka', EntityType::class, [
             'placeholder' => '- Kies een GKA Team -',
             'required' => false,
@@ -63,16 +51,23 @@ class CreateDossierFormType extends AbstractType
             'multiple' => false,
             'expanded' => false
         ]);
+
         $builder->add('regasNummer', TextType::class, [
             'required' => false,
             'label' => 'MaDi dossiernr.'
         ]);
+
         $builder->add('allegroNummer', TextType::class, [
             'required' => false,
             'label' => 'GKA dossiernr.'
         ]);
+
+        $builder->addEventSubscriber($this->medewerkerSchuldhulpbureau);
     }
 
+    /**
+     * @param OptionsResolver $resolver
+     */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefault('data_class', Dossier::class);
