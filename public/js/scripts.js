36183d66c8a35b6d3c16c298050b76ff5d1a9023
Maurice Guikema <>
2019-02-27 16:33:22 +0100

    reload dossier page after dashboard status action selector safe

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1060,8 +1060,11 @@
         newDocCounter = 1,
         i;
 
-
-      e && e.preventDefault();
+      if (form.querySelector('#change_dossier_status_status') && form.querySelector('#change_dossier_status_status').value !== form.querySelector('#change_dossier_status_status').dataset.current){
+        w.onbeforeunload = function () {}
+      }else {
+        e && e.preventDefault();
+      }
 
       form && changers[form.dataset.changer].call(form, e);
       if (form.classList.contains('invalid')){
