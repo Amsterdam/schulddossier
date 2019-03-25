2c1aa1bcc159654da6c348b0858ec52595825c46
Johan Groenen <johan.groenen+github@gmail.com>
2019-03-25 11:54:12 +0100

    Update Version20190318132708.php

--- src/Migrations/Version20190318132708.php
+++ src/Migrations/Version20190318132708.php
@@ -23,7 +23,6 @@ final class Version20190318132708 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE gebruiker DROP telefoonnummer');
     }
 }
