acfdc245459bb732a8aa430fb4798718e2941a97
Roberto Gardenier <roberto@caroga.net>
2019-03-27 13:01:13 +0100

    Mijn gegevens (#122)
    * form handleRequest failing
    
    * fix: dont validate not rendered fields
    
    * Naam not editable
    
    * cleaning up
    
    - removed unused use statements
    - self update (dispatch gebruiker)
    
    * validate max 12 length telefoonnummer
    
    * link in access denied to mijn gegevens
    
    * Update Version20190318132708.php
    
    * display madi phone number in dossier-dashboard
    
    * display gebruiker phone number in gebruiker overview
    
    * edit gebruiker phone number

---
+++ src/Controller/AppMijnGegevensController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace GemeenteAmsterdam\FixxxSchuldhulp\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Gebruiker;
+use GemeenteAmsterdam\FixxxSchuldhulp\Event\ActionEvent;
+use GemeenteAmsterdam\FixxxSchuldhulp\Form\Type\MijnGegevensFormType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/app/mijn-gegevens")
+ */
+class AppMijnGegevensController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction(Request $request, EntityManagerInterface $em, EventDispatcherInterface $eventDispatcher)
+    {
+        $gebruiker = $this->getUser();
+        $form = $this->createForm(MijnGegevensFormType::class, $gebruiker);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $em->flush();
+                $this->addFlash('success', 'Opgeslagen');
+                $eventDispatcher->dispatch(ActionEvent::NAME, ActionEvent::registerGebruikerGewijzigd($gebruiker, $gebruiker));
+                return $this->redirectToRoute('gemeenteamsterdam_fixxxschuldhulp_appmijngegevens_index');
+            } else {
+                foreach ($form->getErrors() as $key => $error) {
+                    $this->addFlash('error', $error->getMessage());
+                }
+            }
+        }
+        return $this->render('MijnGegevens/index.html.twig', [
+            'gebruiker' => $gebruiker,
+            'form' => $form->createView()
+        ]);
+    }
+}
