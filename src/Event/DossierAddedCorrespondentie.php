ee0c020c9e5014a32612c93f928e8ea94c4e8721
Roberto Gardenier <roberto@caroga.net>
2019-04-05 10:09:04 +0200

    feat: dossier section correspondentie added (#143)
    * feat: dossier section correspondentie added
    
    * Will now send an email to the behandelaar when adding new official correspondentie.
    
    * fix: correspondetie style added
    
    * css rendered

---
+++ src/Event/DossierAddedCorrespondentie.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GemeenteAmsterdam\FixxxSchuldhulp\Event;
+
+class DossierAddedCorrespondentie extends DossierChangedEvent
+{
+    const NAME = 'app.dossier.correspondentie.added';
+}
