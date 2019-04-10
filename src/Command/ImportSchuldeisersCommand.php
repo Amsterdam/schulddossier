930b1d61c9911e951c3895bfc5b327b2ff949ee3
mguikema <mguikema@users.noreply.github.com>
2019-04-10 05:44:34 -0700

    Fix/tab order nieuwschulitem (#181)
    * New schuldeisers import will now also mark missing schuldeisers as inactive in the database (#179)
    
    * fix: tab order new schulditem and existing schulditem

--- src/Command/ImportSchuldeisersCommand.php
+++ src/Command/ImportSchuldeisersCommand.php
@@ -2,20 +2,26 @@
 
 namespace GemeenteAmsterdam\FixxxSchuldhulp\Command;
 
+use Doctrine\ORM\EntityRepository;
+use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Schuldeiser;
+use GemeenteAmsterdam\FixxxSchuldhulp\Repository\SchuldeiserRepository;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
-use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Schuldeiser;
-use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class ImportSchuldeisersCommand extends ContainerAwareCommand
 {
+    /**
+     * @var array
+     */
+    private $importedSchuldeisers = [];
+
     protected function configure()
     {
         $this->setName('app:import:schuldeiser');
-        $this->addArgument('fieldList', InputArgument::REQUIRED, 'Volgorde van de kolommen gescheiden met komma\'s, de volgende kolommen moeten worden gedefinieerd: bedrijfsnaam,rekening,allegroCode,straat,huisnummer,huisnummerToevoeging,postcode,plaats,opmerkingen');
+        $this->addArgument('fieldList', InputArgument::REQUIRED, 'Volgorde van de kolommen gescheiden met komma\'s, de volgende kolommen moeten worden gedefinieerd: bedrijfsnaam,rekening,allegroCode,straat,huisnummer,huisnummerToevoeging,postcode,plaats');
         $this->addArgument('file', InputArgument::REQUIRED, 'Locatie van CSV bestand');
         $this->addOption('skipFirstRow', null, InputOption::VALUE_NONE);
         $this->addOption('semicolon', null, InputOption::VALUE_NONE);
@@ -25,19 +31,19 @@ class ImportSchuldeisersCommand extends ContainerAwareCommand
     {
         $delimiter = $input->getOption('semicolon') ? ';' : ',';
 
-        $expectedFields = ['bedrijfsnaam', 'rekening', 'allegroCode', 'straat', 'huisnummer', 'huisnummerToevoeging', 'postcode', 'plaats', 'opmerkingen'];
+        $expectedFields = ['bedrijfsnaam', 'rekening', 'allegroCode', 'straat', 'huisnummer', 'huisnummerToevoeging', 'postcode', 'plaats'];
         $fieldList = explode(',', $input->getArgument('fieldList'));
         foreach ($expectedFields as $expectedField) {
-            if (in_array($expectedField, $fieldList) === false) {
+            if (in_array($expectedField, $fieldList, true) === false) {
                 $output->writeln('Veld: ' . $expectedField . ' mist in veldenlijst');
                 return;
             }
         }
 
-        $f = fopen($input->getArgument('file'), 'r');
+        $f = fopen($input->getArgument('file'), 'rb');
         $i = 0;
-        while($row = fgetcsv($f, null, $delimiter)) {
-            $i ++;
+        while ($row = fgetcsv($f, null, $delimiter)) {
+            $i++;
             $output->writeln('Start line: ' . $i);
             if ($i === 1 && $input->getOption('skipFirstRow')) {
                 $output->writeln('Skip first line');
@@ -46,7 +52,7 @@ class ImportSchuldeisersCommand extends ContainerAwareCommand
 
             $data = [];
             foreach ($expectedFields as $expectedField) {
-                $index = array_search($expectedField, $fieldList);
+                $index = array_search($expectedField, $fieldList, true);
                 if (isset($row[$index])) {
                     $data[$expectedField] = $row[$index];
                 } else {
@@ -63,6 +69,7 @@ class ImportSchuldeisersCommand extends ContainerAwareCommand
         fclose($f);
 
         $this->getContainer()->get('doctrine')->getManager()->flush();
+        $this->markObsoleteItems();
     }
 
     private function getOrCreateSchuldeiser($allegroCode)
@@ -87,10 +94,23 @@ class ImportSchuldeisersCommand extends ContainerAwareCommand
         $object->setBedrijfsnaam($data['bedrijfsnaam']);
         $object->setHuisnummer($data['huisnummer']);
         $object->setHuisnummerToevoeging($data['huisnummerToevoeging']);
-        $object->setOpmerkingen($data['opmerkingen']);
         $object->setPlaats($data['plaats']);
         $object->setPostcode($data['postcode']);
         $object->setRekening($data['rekening']);
         $object->setStraat($data['straat']);
+        $object->setEnabled(true);
+
+
+        $this->importedSchuldeisers[] = $object;
+    }
+
+    private function markObsoleteItems()
+    {
+        /** @var SchuldeiserRepository $schuldeiserRepository */
+        $schuldeiserRepository = $this->getContainer()
+            ->get('doctrine')
+            ->getRepository(Schuldeiser::class);
+
+        $schuldeiserRepository->markMissingSchuldeisersInactive($this->importedSchuldeisers)->getQuery()->execute();
     }
 }
