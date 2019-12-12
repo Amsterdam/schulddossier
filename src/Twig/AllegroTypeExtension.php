19b4eb6e650547a39dd29862163f4d4180125bc9
Bart Elferink <bart@soepel.it>
2019-12-12 10:46:10 +0100

    display allegro status

---
+++ src/Twig/AllegroTypeExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GemeenteAmsterdam\FixxxSchuldhulp\Twig;
+
+use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Dossier;
+
+/**
+ * Class AllegroTypeExtension
+ * @package GemeenteAmsterdam\FixxxSchuldhulp\Twig
+ */
+class AllegroTypeExtension extends \Twig_Extension
+{
+    /**
+     * @return array|\Twig_Filter[]s
+     */
+    public function getFilters(): array
+    {
+        return [
+            new \Twig_Filter('allegro_status', function (String $status) {
+                return Dossier::twigAllegroStatus($status);
+            })
+        ];
+    }
+
+}
