d7afa5c31de0dd21f7261ede2595056058910f51
Tim Fierens <t.fierens@amsterdam.nl>
2022-05-18 16:02:31 +0200

    added sociaal dns entries back to hosts file

---
+++ docker-entrypoint_phpfpm.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# these hosts are not on the amsterdam DNS server
+echo "10.204.22.40 schuldhulp-ft.sociaal.amsterdam.nl" >> /etc/hosts
+echo "10.204.22.39 schuldhulp.sociaal.amsterdam.nl" >> /etc/hosts
+
+php-fpm
