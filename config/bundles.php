fa907f591f4c1f73a5a8cfa273975ef3f1799a81
amas <a.acunasanchez@amsterdam.nl>
2022-11-25 10:52:26 +0100

    Upgrade guzzle 6 to guzzle 7

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
 ];
