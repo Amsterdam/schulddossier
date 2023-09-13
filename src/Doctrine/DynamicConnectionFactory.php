c4587fb964db765450d5647cbabf61fae93560cb
Thomas van Oort <t.van.oort@amsterdam.nl>
2023-09-13 08:50:41 +0200

    took over settings from Adus for Azure DB

---
+++ src/Doctrine/DynamicConnectionFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Doctrine;
+
+use App\Azure\AzureDatabase;
+use Doctrine\Bundle\DoctrineBundle\ConnectionFactory as BaseConnectionFactory;
+use Doctrine\Common\EventManager;
+use Doctrine\DBAL\Configuration;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class DynamicConnectionFactory extends BaseConnectionFactory
+{
+    public function __construct(private ContainerInterface $container, private AzureDatabase $azureDatabase)
+    {
+        parent::__construct($container->getParameter('doctrine.dbal.connection_factory.types'));
+    }
+
+    public function createConnection(array $params, ?Configuration $config = null, ?EventManager $eventManager = null, array $mappingTypes = []): DynamicConnection
+    {
+
+        $defaultConnection = parent::createConnection($params, $config, $eventManager, $mappingTypes);
+
+        $driver = $defaultConnection->getDriver();
+
+        return new DynamicConnection($defaultConnection->getParams(), $driver, $defaultConnection->getConfiguration(), $defaultConnection->getEventManager(), $this->azureDatabase);
+    }
+}
