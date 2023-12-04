046a8370651b58fe8abe2fb948cfd94ae34ac5c6
Thomas van Oort <t.van.oort@amsterdam.nl>
2023-12-04 11:52:23 +0100

    Update DB for rotating password

--- src/Azure/AzureDatabase.php
+++ src/Azure/AzureDatabase.php
@@ -1,22 +1,39 @@
 <?php
 
-namespace GemeenteAmsterdam\FixxxSchuldhulp\Azure;
+namespace App\Azure;
 
-use GuzzleHttp\Exception\RequestException;
+use Exception;
 use GuzzleHttp\RequestOptions;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\Adapter\PdoAdapter;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class AzureDatabase
 {
-    public function __construct(private HttpClientInterface $client, private string $azureAuthorityHost, private string $azureTenantId, private string $azureFederatedTokenFile, private string $azureClientId)
+    public const CACHE_KEY = 'azure-db-password';
+
+    public function __construct(private HttpClientInterface $client, private LoggerInterface $logger, private string $azureAuthorityHost, private string $azureTenantId, private string $azureFederatedTokenFile, private string $azureClientId)
     {
+
     }
 
-    public function getPassword(string $default): string
+    public function getPassword(string $default, $invalidateCache = false): string
     {
-        if (!$this->azureAuthorityHost || !$this->azureTenantId || !$this->azureFederatedTokenFile || !$this->azureClientId) return $default;
+        $this->logger->debug("Fetching DB Password either from cache or filesystem");
+        $cache = new FilesystemAdapter();
+        if ($invalidateCache) {
+            $cache->delete(self::CACHE_KEY);
+        }
 
-        return $this->getPasswordFromAzure();
+        return $cache->get(self::CACHE_KEY, function (ItemInterface $item) use ($default) {
+            $this->logger->debug("Cache invalid. Getting db password from azure");
+            if (!$this->azureAuthorityHost || !$this->azureTenantId || !$this->azureFederatedTokenFile || !$this->azureClientId) return $default;
+
+            return $this->getPasswordFromAzure();
+        });
     }
 
     private function getPasswordFromAzure(): string
@@ -38,33 +55,19 @@ class AzureDatabase
             'client_id' => $clientId
         ];
 
+        $response = $this->client->request('POST', $tokenUrl, [RequestOptions::HEADERS => ['Content-Type' => 'application/x-www-form-urlencoded'], RequestOptions::BODY => http_build_query($payload)]);
 
-//        try {
-            $response = $this->client->request('POST', $tokenUrl, [RequestOptions::HEADERS => ['Content-Type' => 'application/x-www-form-urlencoded'], RequestOptions::BODY => http_build_query($payload)]);
+        if ($response->getStatusCode() >= 400) {
+            throw new Exception(json_encode(['url' => $tokenUrl, 'payload' => $payload, 'response' => $response->getContent(false), 'fullresponse' => $response->toArray(false)]));
+        }
 
-            if ($response->getStatusCode() >= 400) {
-                throw new \Exception(json_encode(['url' => $tokenUrl, 'payload' => $payload, 'response' => $response->getContent(false), 'fullresponse' => $response->toArray(false)]));
-            }
+        $body = $response->getContent(false);
+        $data = json_decode($body, true);
 
-            $body = $response->getContent(false);
-            $data = json_decode($body, true);
+        $accessToken = $data['access_token'];
 
-            $accessToken = $data['access_token'];
+        $this->logger->debug("Succesfully got password from Azure!");
 
-            return $accessToken;
-
-//        } catch (RequestException $e) {
-//            // Handle the request exception
-//            if ($e->hasResponse()) {
-//                $response = $e->getResponse();
-//                $body = $response->getBody();
-//                $statusCode = $response->getStatusCode();
-//
-//                echo "ERROR: $body";
-//                // Handle the error response...
-//            }
-//        }
-
-        return '';
+        return $accessToken;
     }
 }
