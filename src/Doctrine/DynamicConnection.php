ee935fc4a4c24f1453c865b67e50df0ea8c2f67f
Thomas van Oort <t.van.oort@amsterdam.nl>
2023-12-04 11:52:23 +0100

    Update DB for rotating password

--- src/Doctrine/DynamicConnection.php
+++ src/Doctrine/DynamicConnection.php
@@ -1,29 +1,51 @@
 <?php
 
-namespace GemeenteAmsterdam\FixxxSchuldhulp\Doctrine;
+namespace App\Doctrine;
 
-use GemeenteAmsterdam\FixxxSchuldhulp\Azure\AzureDatabase;
+use App\Azure\AzureDatabase;
 use Doctrine\Common\EventManager;
 use Doctrine\DBAL\Configuration;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Driver;
+use Psr\Log\LoggerInterface;
 
 class DynamicConnection extends Connection
 {
     public function __construct(
-        array          $params,
-        Driver         $driver,
-        ?Configuration $config = null,
-        ?EventManager  $eventManager = null,
-        ?AzureDatabase $azureDatabase = null,
+        array                             $params,
+        Driver                            $driver,
+        ?Configuration                    $config = null,
+        ?EventManager                     $eventManager = null,
+        private readonly ?AzureDatabase   $azureDatabase = null,
+        private readonly ?LoggerInterface $logger = null,
     )
     {
-        if ($azureDatabase) {
-            $params['password'] = $azureDatabase->getPassword($params['password']);
+        if ($azureDatabase && $logger && isset($params['password'])) {
+            $newPassword = $azureDatabase->getPassword($params['password']);
+            $params = $this->addNewPasswordToParams($params, $newPassword);
         }
 
-
-
         parent::__construct($params, $driver, $config, $eventManager);
+
+        if ($azureDatabase && $logger && isset($params['password'])) {
+            try {
+                $this->connect();
+            } catch (\Exception $e) {
+                $this->logger->debug("DB Connection failed. Trying to invalidate cache and set password again.");
+                $newPassword = $azureDatabase->getPassword($params['password'], true);
+                $params = $this->addNewPasswordToParams($params, $newPassword);
+                parent::__construct($params, $driver, $config, $eventManager);
+                $this->connect();
+            }
+        }
     }
+
+    private function addNewPasswordToParams(array $params, string $newPassword): array
+    {
+        $params['password'] = $newPassword;
+        $params['url'] = str_replace('temporary', $newPassword, $params['url']);
+
+        return $params;
+    }
+
 }
