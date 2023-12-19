207bc607e7a83800da4a2c203ed3cc9aa2eefe6f
Thomas van Oort <t.van.oort@amsterdam.nl>
2023-12-19 15:51:31 +0100

    added comment

--- src/Azure/Config/SASFileReaderConfig.php
+++ src/Azure/Config/SASFileReaderConfig.php
@@ -22,6 +22,7 @@ class SASFileReaderConfig
                 // currently signed to the container, but could also be signed to specific blob (b).
                 'signedResource' => 'b',
                 'apiVersion' => '2023-01-01',
+                // TODO: Check timezones, Azure was running at -1 hour compared to local
                 'expiry' => (new \DateTime('now + 120 minutes'))->format('Y-m-d\TH:i:s\Z'),
                 'start' => (new \DateTime('120 minutes ago'))->format('Y-m-d\TH:i:s\Z'),
             ]
