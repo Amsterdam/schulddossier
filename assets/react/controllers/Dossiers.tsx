309f0d9c8817265233d4a18e01d2c9db3d686911
Ruben Sibon <r.sibon@amsterdam.nl>
2025-10-21 16:33:05 +0200

    feat(React): correct routing, security & auth

--- assets/react/controllers/Dossiers.tsx
+++ assets/react/controllers/Dossiers.tsx
@@ -2,6 +2,6 @@ interface DossiersProps {
     fullName: string;
 }
 
-export default function Dossiers(props: DossiersProps) {
+export default function Dossiers(props: Readonly<DossiersProps>) {
     return <div>Dossier van {props.fullName}</div>;
 }
--- src/Controller/React/DossierController.php
+++ src/Controller/React/DossierController.php
@@ -2,14 +2,15 @@
 
 namespace GemeenteAmsterdam\FixxxSchuldhulp\Controller\React;
 
-use Symfony\Component\ExpressionLanguage\Expression;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\ExpressionLanguage\Expression;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted(attribute: new Expression("is_granted('ROLE_ADMIN')"))]
 class DossierController extends AbstractController
 {
-    #[\Symfony\Component\Routing\Attribute\Route(path: '/dossiers/')]
-    #[IsGranted(attribute: new Expression("is_granted('ROLE_ADMIN')"))]
+    #[Route(path: '/dossiers/')]
     public function index()
     {
         return $this->render('React/Dossier/index.html.twig');
