175df30bf191b810a6806130513305fa976e1d87
Ruben Sibon <r.sibon@amsterdam.nl>
2025-10-24 11:02:47 +0200

    build(React): config w/ seperate React app entry

--- assets/app.js
+++ assets/app.js
@@ -5,25 +5,13 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-import { registerReactControllerComponents } from '@symfony/ux-react';
-
-// start the Stimulus application
-import './bootstrap.js';
-
-// other scripts
-import './js/flashMessage.js'
-import './js/scripts.js'
-
-// scss
+// SCSS
 import './scss/screen.scss';
-
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css'
 
 window.pdfjsLib = require("pdfjs-dist/webpack");
 window.Sortable = require("sortablejs");
 window.jsPDF = require("jspdf").jsPDF;
 
-registerReactControllerComponents(
-  require.context('./react/controllers', true, /\.[jt]sx?$/)
-);
+// Other scripts
+import './js/flashMessage.js'
+import './js/scripts.js'
