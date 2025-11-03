9350efe5e2d8b64148b644a5895e146076269492
jmassink <j.massink@amsterdam.nl>
2025-11-03 16:47:33 +0100

    fixture: update indiendatum to dossier fixtures

--- src/DataFixtures/DossierFixtures.php
+++ src/DataFixtures/DossierFixtures.php
@@ -45,8 +45,10 @@ class DossierFixtures extends \Doctrine\Bundle\FixturesBundle\Fixture implements
             $dossier->setEersteKeerVerzondenAanGKA($dossiers[$i]['verzondenGka']);
             $dossier->setInPrullenbak($dossiers[$i]['inPrullenbak']);
 
+
             if($dossiers[$i]['verzondenGka']) {
                 $dossier->setAllegroNummer(834 . ($i-1) . 3879 . $i);
+                $dossier->setIndienDatumTijd(new DateTime('now'));
             }
 
             $this->addReference($i === 0 ? 'dossier' : 'dossier'.$i, $dossier);
