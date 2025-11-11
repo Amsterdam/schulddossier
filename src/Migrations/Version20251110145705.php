8599fe542777725e3b5cc33759e25ea50edc3ef6
jmassink <j.massink@amsterdam.nl>
2025-11-11 09:39:48 +0100

    Feat!: migration from indientDatum dossiers

---
+++ src/Migrations/Version20251110145705.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20251110145705 extends AbstractMigration
+{
+  public function getDescription(): string
+    {
+        return 'Update dossier.indiendatum_tijd with the latest datumtijd from dossier_timeline for subtype verzenden_gka';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql("
+            UPDATE dossier
+            SET indiendatum_tijd = (
+                SELECT MAX(dt.datumtijd)
+                FROM dossier_timeline dt
+                WHERE dt.dossier_id = dossier.id
+                  AND dt.subtype = 'verzenden_gka'
+            )
+            WHERE EXISTS (
+                SELECT 1
+                FROM dossier_timeline dt
+                WHERE dt.dossier_id = dossier.id
+                  AND dt.subtype = 'verzenden_gka'
+            );
+        ");
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql("
+            UPDATE dossier
+            SET indiendatum_tijd = NULL
+        ");
+    }
+}
