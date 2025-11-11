ea6dd3e5ca6ee94f58e201f04bb9e815f558379e
Jasper Massink <85297826+jmassink@users.noreply.github.com>
2025-11-11 16:48:27 +0100

    feat(SCHUL-365)!: DB migration "Datum van indienen" (#355)
    * Feat!: migration from indientDatum dossiers

---
+++ src/Migrations/Version20251111152154.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251111152154 extends AbstractMigration
+{
+  public function getDescription(): string
+    {
+        return 'Update dossier.indiendatum_tijd with the latest datumtijd from dossier_timeline for subtype verzenden_shv';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql("
+            UPDATE dossier
+            SET indiendatum_tijd = (
+                SELECT MAX(dt.datumtijd)
+                FROM dossier_timeline dt
+                WHERE dt.dossier_id = dossier.id
+                  AND dt.subtype = 'verzenden_shv'
+            )
+            WHERE EXISTS (
+                SELECT 1
+                FROM dossier_timeline dt
+                WHERE dt.dossier_id = dossier.id
+                  AND dt.subtype = 'verzenden_shv'
+            );
+        ");
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql("
+            UPDATE dossier
+            SET indiendatum_tijd = NULL
+        ");
+    }
+}
