757848a1d1b90eb967ece4df17f1ecf7a09ff719
jmassink <j.massink@amsterdam.nl>
2026-03-09 13:48:08 +0100

    improv(transformDossierLabel): improve phpdoc

--- src/Twig/TransformDossierLabel.php
+++ src/Twig/TransformDossierLabel.php
@@ -7,14 +7,21 @@ namespace GemeenteAmsterdam\FixxxSchuldhulp\Twig;
 use GemeenteAmsterdam\FixxxSchuldhulp\Constants\DossierFormLabel;
 
 /**
- * Class GebruikerTypeToTitleExtension
+ * Class TransformDossierLabel
+ *
+ * A Twig extension that provides custom filters for transforming dossier labels.
+ * This extension is used to make keys more human-readable by applying a transformation
+ * using the DossierFormLabel::getFormLabelOrHumanize() method
  *
  * @package GemeenteAmsterdam\FixxxSchuldhulp\Twig
  */
 class TransformDossierLabel extends \Twig_Extension
 {
     /**
-     * @return array|\Twig_Filter[]s
+     * The filter 'transform_dossier_label' transforms a key into a readable label
+     * using the method DossierFormLabel::getFormLabelOrHumanize().
+     *
+     * @return \Twig_Filter[] An array of Twig filters.
      */
 
     public function getFilters(): array
