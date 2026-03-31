87214bf8641f405700ad3688764a9a3cebccdf27
jmassink <j.massink@amsterdam.nl>
2026-03-31 16:04:52 +0200

    more explicit separate code

--- src/Allegro/LoginClientFactory.php
+++ src/Allegro/LoginClientFactory.php
@@ -5,8 +5,10 @@ namespace GemeenteAmsterdam\FixxxSchuldhulp\Allegro;
 use GemeenteAmsterdam\FixxxSchuldhulp\Allegro\Login\AllegroLoginClassmap;
 use GemeenteAmsterdam\FixxxSchuldhulp\Allegro\Login\AllegroLoginClient;
 use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Organisatie;
+use Http\Adapter\Guzzle7\Client;
 use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapEngineFactory;
 use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapOptions;
+use Phpro\SoapClient\Soap\Handler\HttPlugHandle;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class LoginClientFactory
@@ -17,17 +19,43 @@ class LoginClientFactory
         ?string $proxyHostIp = null,
         ?string $proxyHostPort = null
     ): AllegroLoginClient {
-        $extSoapOptionsArray = AllegroHelper::createSoapOptionsArray($proxyHostIp, $proxyHostPort);
 
-        $handler = AllegroHelper::createSoapClientHandler($organisatie, $proxyHostIp, $proxyHostPort);
+        if (isset($proxyHostIp, $proxyHostPort)) {
+
+            $extSoapOptionsArray = AllegroHelper::createSoapOptionsArray($proxyHostIp, $proxyHostPort);
+
+            $handler = AllegroHelper::createSoapClientHandler($organisatie, $proxyHostIp, $proxyHostPort);
+
+            $engine = ExtSoapEngineFactory::fromOptionsWithHandler(
+                ExtSoapOptions::defaults($wsdl, $extSoapOptionsArray)->withClassMap(
+                    AllegroLoginClassmap::getCollection()
+                ),
+                $handler
+            );
+            $eventDispatcher = new EventDispatcher();
+
+
+            return new AllegroLoginClient($engine, $eventDispatcher);
+        }
+
+
+        $handler = HttPlugHandle::createForClient(
+            Client::createWithConfig(['headers' => ['User-Agent' => 'fixxx-schuldhulp/1.0']])
+        );
+
+
+        if (null !== $organisatie) {
+            $handler->addMiddleware(new SessionMiddleware($organisatie));
+        }
 
         $engine = ExtSoapEngineFactory::fromOptionsWithHandler(
-            ExtSoapOptions::defaults($wsdl, $extSoapOptionsArray)->withClassMap(
-                AllegroLoginClassmap::getCollection()
-            ),
+            ExtSoapOptions::defaults($wsdl, [])->withClassMap(AllegroLoginClassmap::getCollection()),
             $handler
         );
         $eventDispatcher = new EventDispatcher();
+
+        return new AllegroLoginClient($engine, $eventDispatcher);
+
 
         return new AllegroLoginClient($engine, $eventDispatcher);
     }
--- src/Allegro/SchuldHulpClientFactory.php
+++ src/Allegro/SchuldHulpClientFactory.php
@@ -8,6 +8,8 @@ use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Organisatie;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapEngineFactory;
 use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapOptions;
+use Http\Adapter\Guzzle7\Client;
+use Phpro\SoapClient\Soap\Handler\HttPlugHandle;
 
 class SchuldHulpClientFactory
 {
@@ -17,15 +19,33 @@ class SchuldHulpClientFactory
         ?string $proxyHostIp = null,
         ?string $proxyHostPort = null
     ): AllegroSchuldHulpClient {
-        $handler = AllegroHelper::createSoapClientHandler($organisatie, $proxyHostIp, $proxyHostPort);
 
-        $extSoapOptions = AllegroHelper::createSoapOptionsArray($proxyHostIp, $proxyHostPort);
+        if (isset($proxyHostIp, $proxyHostPort)) {
 
-        $engine = ExtSoapEngineFactory::fromOptionsWithHandler(
-            ExtSoapOptions::defaults($wsdl, $extSoapOptions)->withClassMap(AllegroSchuldHulpClassmap::getCollection()),
-            $handler
+            $handler = AllegroHelper::createSoapClientHandler($organisatie, $proxyHostIp, $proxyHostPort);
+
+            $extSoapOptions = AllegroHelper::createSoapOptionsArray($proxyHostIp, $proxyHostPort);
+
+            $engine = ExtSoapEngineFactory::fromOptionsWithHandler(
+                ExtSoapOptions::defaults($wsdl, $extSoapOptions)->withClassMap(AllegroSchuldHulpClassmap::getCollection()),
+                $handler
+            );
+
+            $eventDispatcher = new EventDispatcher();
+
+            return new AllegroSchuldHulpClient($engine, $eventDispatcher);
+        }
+
+        $handler = HttPlugHandle::createForClient(
+            Client::createWithConfig(['headers' => ['User-Agent' => 'fixxx-schuldhulp/1.0']])
         );
 
+        $handler->addMiddleware(new SessionMiddleware($organisatie));
+
+        $engine = ExtSoapEngineFactory::fromOptionsWithHandler(
+            ExtSoapOptions::defaults($wsdl, [])->withClassMap(AllegroSchuldHulpClassmap::getCollection()),
+            $handler
+        );
         $eventDispatcher = new EventDispatcher();
 
         return new AllegroSchuldHulpClient($engine, $eventDispatcher);
