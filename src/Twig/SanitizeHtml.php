3e50613fb54cb6192979e8c2512ba01195d15b9e
Jasper Massink <85297826+jmassink@users.noreply.github.com>
2026-03-31 13:31:49 +0200

    feat(schul-956): algemene tekst 'dossier gewijzigd' alleen als er geen remark is (#432)

---
+++ src/Twig/SanitizeHtml.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GemeenteAmsterdam\FixxxSchuldhulp\Twig;
+use HTMLPurifier;
+use HTMLPurifier_Config;
+
+use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Dossier;
+
+/**
+ * Class SanitizeHtml
+ *
+ * This class extends Twig's functionality by adding a custom filter to sanitize HTML input.
+ * It uses the HTMLPurifier library to allow only specific HTML tags (e.g., <strong>).
+ */
+class SanitizeHtml extends \Twig_Extension
+{
+    /**
+     * @return array|\Twig_Filter[]
+     */
+    public function getFilters(): array
+    {
+        return [
+            new \Twig_Filter('sanitize', function ($input) {
+                $config = HTMLPurifier_Config::createDefault();
+                $config->set('HTML.Allowed', 'strong, br');
+                $purifier = new HTMLPurifier($config);
+                $purifiedInput =  $purifier->purify($input);
+
+                return $purifiedInput;
+            })
+        ];
+    }
+}
