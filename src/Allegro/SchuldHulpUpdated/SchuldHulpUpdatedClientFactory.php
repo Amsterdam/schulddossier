04d18b714ad6c3f5ba854720a164dbb5df260dd2
jmassink <j.massink@amsterdam.nl>
2026-04-17 12:58:37 +0200

    feat(allegro): implement psr18 transport middleware

--- src/Allegro/SchuldHulpUpdated/SchuldHulpUpdatedClientFactory.php
+++ src/Allegro/SchuldHulpUpdated/SchuldHulpUpdatedClientFactory.php
@@ -3,11 +3,8 @@
 namespace GemeenteAmsterdam\FixxxSchuldhulp\Allegro\SchuldHulpUpdated;
 
 use GemeenteAmsterdam\FixxxSchuldhulp\Allegro\AllegroHelper;
-use GemeenteAmsterdam\FixxxSchuldhulp\Allegro\SchuldHuldUpdated\SchuldHulpUpdatedClient;
-use GemeenteAmsterdam\FixxxSchuldhulp\Allegro\SchuldHuldUpdated\SchuldHulpUpdatedClassmap;
 use GemeenteAmsterdam\FixxxSchuldhulp\Allegro\SessionMiddleware;
 use GemeenteAmsterdam\FixxxSchuldhulp\Entity\Organisatie;
-use GuzzleHttp\Client;
 use Http\Client\Common\PluginClient;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Phpro\SoapClient\Soap\DefaultEngineFactory;
@@ -16,9 +13,14 @@ use Phpro\SoapClient\Caller\EventDispatchingCaller;
 use Phpro\SoapClient\Caller\EngineCaller;
 use Soap\Psr18Transport\Psr18Transport;
 use Http\Discovery\Psr18ClientDiscovery;
+use Soap\Psr18Transport\Middleware\SoapHeaderMiddleware;
 use Soap\Xml\Builder\SoapHeader;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpClient\Psr18Client;
 
+use function VeeWee\Xml\Dom\Builder\children;
+use function VeeWee\Xml\Dom\Builder\namespaced_element;
+use function VeeWee\Xml\Dom\Builder\value;
 
 class SchuldHulpUpdatedClientFactory
 {
@@ -28,24 +30,43 @@ class SchuldHulpUpdatedClientFactory
         ?string $proxyHostIp = null,
         ?string $proxyHostPort = null
     ): SchuldHulpUpdatedClient {
+        // TODO handler implementeren: https://gemeente-amsterdam.atlassian.net/browse/SCHUL-994
+        // needs become a transporter: https://github.com/phpro/soap-client/blob/v2.0.0/UPGRADING.md#transport
+        // $handler = AllegroHelper::createSoapClientHandler($organisatie, $proxyHostIp, $proxyHostPort);
+
+        /* if the code does not work, we could try this one */
+        // $httpClient = Psr18ClientDiscovery::find();
+
 
         if (!empty($proxyHostIp) && !empty($proxyHostPort)) {
-            // TODO handler implementeren: https://gemeente-amsterdam.atlassian.net/browse/SCHUL-994
-            // needs become a transporter: https://github.com/phpro/soap-client/blob/v2.0.0/UPGRADING.md#transport
-            // $handler = AllegroHelper::createSoapClientHandler($organisatie, $proxyHostIp, $proxyHostPort);
 
             $httpClient = HttpClient::create([
-                'proxy' => 'http://' . $proxyHostIp . ':' . $proxyHostPort
+                'proxy' => 'http://' . $proxyHostIp . ':' . $proxyHostPort,
+                'headers' => [
+                    'User-Agent' => 'fixxx-schuldhulp/1.0'
+                ]
             ]);
 
+            $psr18Client = new Psr18Client($httpClient);
+
+            $tns = 'http://tempuri.org/';
             $transporter = Psr18Transport::createForClient(
                 new PluginClient(
-                    $httpClient,
+                    $psr18Client,
                     [
-                        new SoapHeader('User-Agent', 'fixxx-schuldhulp/1.0'),
+                        new SoapHeaderMiddleware(
+                            new SoapHeader(
+                                $tns,
+                                'ROClientIDHeader',
+                                children(
+                                    namespaced_element($tns, 'ID', value($organisatie->getAllegroSessionId())),
+                                )
+                            )
+                        )
                     ]
                 )
             );
+
 
             $extSoapOptions = AllegroHelper::createSoapOptionsArray($proxyHostIp, $proxyHostPort);
 
@@ -60,18 +81,32 @@ class SchuldHulpUpdatedClientFactory
             return new SchuldHulpUpdatedClient($caller);
         }
 
-        $httpClient = Psr18ClientDiscovery::find();
 
+        $httpClient = HttpClient::create([
+            'headers' => [
+                'User-Agent' => 'fixxx-schuldhulp/1.0'
+            ]
+        ]);
+
+        $psr18Client = new Psr18Client($httpClient);
+
+        $tns = 'http://tempuri.org/';
         $transporter = Psr18Transport::createForClient(
             new PluginClient(
-                $httpClient,
+                $psr18Client,
                 [
-                    new SoapHeader('User-Agent', 'fixxx-schuldhulp/1.0'),
-                    new SessionMiddleware($organisatie)
+                    new SoapHeaderMiddleware(
+                        new SoapHeader(
+                            $tns,
+                            'ROClientIDHeader',
+                            children(
+                                namespaced_element($tns, 'ID', value($organisatie->getAllegroSessionId())),
+                            )
+                        )
+                    )
                 ]
             )
         );
-
 
         $engine = DefaultEngineFactory::create(
             ExtSoapOptions::defaults($wsdl, [])
